7727c5fb291eedd8f2310dc6244b2a1421bab9a1
rajarajagopi <rajagopimca@gmail.com>
2023-07-13 13:22:24 +0530

    To Compare JSON String Properties Length not Equal to Same

---
+++ Taskday4.1.js
@@ -0,0 +1,19 @@
+// To Compare JSON String Properties Length not Equal to Same
+var obj1 = {"Name":"Rajagopi","age":"33","phone":9952116933,"address":"Mudukulathur Ramanathapuram","pincode":623704,"bloodgroup":"B+","Education":"MCA","Experience":"4years"};
+var obj2 = {"age":"33","address":"Mudukulathur Ramanathapuram","bloodgroup":"B+","pincode":623704,"Education":"MCA"};
+var flag=true;
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log("JSON String Object IS Not Equal to same:" +flag);
