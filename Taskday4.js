3323d6b87d69390502506a0c37bfe7162605dbaf
rajarajagopi <rajagopimca@gmail.com>
2023-07-14 17:13:18 +0530

    Task day4 Updated

--- Taskday4.js
+++ Taskday4.js
@@ -1,8 +1,6 @@
 var obj1 = {"Name":"Rajagopi","age":"33","phone":9952116933,"address":"Mudukulathur Ramanathapuram","pincode":623704,"bloodgroup":"B+","Education":"MCA","Experience":"4years"};
 var obj2 = {"age":"33","Name":"Rajagopi","address":"Mudukulathur Ramanathapuram","phone":9952116933,"bloodgroup":"B+","pincode":623704,"Experience":"4years","Education":"MCA"};
-
 var flag=true;
-
 if(Object.keys(obj1).length==Object.keys(obj2).length){
     for(key in obj1) { 
         if(obj1[key] == obj2[key]) {
@@ -18,3 +16,23 @@ else {
     flag=false;
 }
 console.log("is object equal"+flag);
+
+// To Compare JSON String Properties Length not Equal to Same
+var obj1 = {"Name":"Rajagopi","age":"33","phone":9952116933,"address":"Mudukulathur Ramanathapuram","pincode":623704,"bloodgroup":"B+","Education":"MCA","Experience":"4years"};
+var obj2 = {"age":"33","address":"Mudukulathur Ramanathapuram","bloodgroup":"B+","pincode":623704,"Education":"MCA"};
+var flag=true;
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log("JSON String Object IS Not Equal to same:" +flag);
